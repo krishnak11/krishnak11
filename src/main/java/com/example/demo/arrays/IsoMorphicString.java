e68108f01c9a0d17cc4c05719392c5913c320864
Krishna Kshetri <krishnakshetri@krishnas-air.attlocal.net>
2020-04-03 22:24:13 -0500

    isomorphic string solution

---
+++ src/main/java/com/example/demo/arrays/IsoMorphicString.java
@@ -0,0 +1,13 @@
+package com.example.demo.arrays;
+
+public class IsoMorphicString {
+
+    public boolean isIsoMorphic(String s, String t){
+        char [] sArray = s.toCharArray();
+        char[] tArray = new char[t.length()];
+        for(int i =0; i<t.length();i++){
+
+        }
+        return false;
+    }
+}
